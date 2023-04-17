702507a4278c4ae8ae0c95156e6d8d0f4c059b2a
Lucas <lucasrocini@hotmail.com>
2023-04-16 22:21:39 -0300

    Login Page and Login/Logoff working

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-import {View} from 'react-native';
+import {View, ActivityIndicator} from 'react-native';
 
 import AppRoutes from './app.routes';
 import AuthRoutes from './auth.routes';
 
+import { AuthContext } from '../contexts/AuthContext'
+
 function Routes(){
-  const isAuthenticated = false;
+  const { isAuthenticated } = useContext(AuthContext);
   const loading = false;
+
+  if(loading){
+    return(
+      <View 
+        style={{ 
+          flex:1, 
+          backgroundColor: '#1D1D2E', 
+          justifyContent: 'center', 
+          alignItems:'center' 
+        }}
+      >
+        <ActivityIndicator size={60} color="#FFF" />
+      </View>
+    )
+  }
 
 
   return(
